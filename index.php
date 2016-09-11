963bacdccf3165189d42c951f1f0e83f26c49625
Aman Dayal <12amandayal12@gmail.com>
2016-09-11 19:04:32 +0530

    my third commit

--- index.php
+++ index.php
@@ -1,9 +1,38 @@
 <html>
 <head>
 <title> My First Task</title>
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
 </head>
 <body>
-<p>Hello! Welcome to this world</p>
-<p>My first web development progrm</p>
+<div class="container">
+<div class="jumbotron">
+<h2 style="font-family:monotype corosiva;"><i>Login Form</i></h2></div></div>
+<form>
+  
+    <div class="container">
+    <div class="col-xs-0">
+      <label for="email">Email:</label>
+      
+      <input type="email" class="container" id="email" placeholder="Enter email"></div></div>
+    </br>
+</form>
+<form>
+    
+     <div class="container">
+     <div class="col-xs-0">
+      <label for="pwd">Password:</label>
+   
+      <input type="password" class="container" id="pwd" placeholder="Enter password"></div></div>
+      </br>
+
+    <div class="container">
+    <button type="submit" class="btn btn-primary">Submit</button></br></br>
+    <label for="pwd">Don't have your Account?</label>
+    <a href="#" class="btn btn-danger btn-DEFAULT active" role="button">Sign Up</a></div>
+  </form>
+
+
+
 </body>
+
 </html>
